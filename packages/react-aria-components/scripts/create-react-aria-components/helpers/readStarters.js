7e61bc360d04443ad17915623676293de934e898
Reid Barber <reid@reidbarber.com>
2024-01-11 17:15:49 -0600

    rename function

--- packages/react-aria-components/scripts/create-react-aria-components/helpers/readStarters.js
+++ packages/react-aria-components/scripts/create-react-aria-components/helpers/readStarters.js
@@ -1,6 +1,6 @@
 import got from 'got';
 
-export const readTemplates = async () => {
+export const readStarters = async () => {
   let res = await got('https://api.github.com/repos/adobe/react-spectrum/contents/starters?ref=create-react-aria-components').catch((e) => e);
   // TODO handle error
   return JSON.parse(res.body).map((t) => t.name);
